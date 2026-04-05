5b8dd7d3df1de6c46d89d56bd67db294322debad
Yang Li <czrocklee@gmail.com>
2026-04-06 00:54:10 +0900

    refactor: extract add_gperf_header into cmake/Gperf.cmake

---
+++ cmake/Gperf.cmake
@@ -0,0 +1,27 @@
+# SPDX-License-Identifier: MIT
+# Copyright (c) 2024-2025 RockStudio Contributors
+
+# Gperf header generation function.
+# Generates a header file from a gperf input file.
+
+function(add_gperf_header out_var input relative_output)
+  set(output "${CMAKE_CURRENT_BINARY_DIR}/generated/${relative_output}")
+  get_filename_component(output_dir "${output}" DIRECTORY)
+
+  add_custom_command(
+    OUTPUT "${output}"
+    COMMAND "${CMAKE_COMMAND}" -E make_directory "${output_dir}"
+    COMMAND
+      "${CMAKE_COMMAND}"
+      -DGPERF_EXECUTABLE=${GPERF_EXECUTABLE}
+      -DINPUT=${input}
+      -DOUTPUT=${output}
+      -P "${CMAKE_CURRENT_SOURCE_DIR}/cmake/RunGperf.cmake"
+    DEPENDS
+      "${input}"
+      "${CMAKE_CURRENT_SOURCE_DIR}/cmake/RunGperf.cmake"
+    VERBATIM
+  )
+
+  set(${out_var} "${output}" PARENT_SCOPE)
+endfunction()
